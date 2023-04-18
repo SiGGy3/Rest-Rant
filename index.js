a132dd92455b1179fb78351017490d99f32f4eda
SiGGy3 <justinjwsig@gmail.com>
2023-04-18 10:49:32 -0700

    added 404 page

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.get('*', (req, res) =>
+{
+    res.status(404).send('<h1>404, page not found</h1>')
+})
+
 app.listen(process.env.PORT)
