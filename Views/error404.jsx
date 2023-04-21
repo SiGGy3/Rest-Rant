7c2185d90100d7efcb43d1d0c47572d8146e7560
SiGGy3 <justinjwsig@gmail.com>
2023-04-20 21:04:33 -0700

    added bootstrap grid to index page

--- Views/error404.jsx
+++ Views/error404.jsx
@@ -6,6 +6,7 @@ function error404 () {
     <Def>
         <main>
             <h1>404: PAGE NOT FOUND</h1>
+            <img src="\images\404img.jpg" alt="404 image"/>
             <p> Oops! Sorry, but this page cannot be found!</p>
         </main>
     </Def>
--- Views/home.jsx
+++ Views/home.jsx
@@ -1,18 +1,25 @@
 const React = require('react')
 const Def = require('./default')
 
-function home () {
-    return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-              <a href="/places">
-  <button className="btn-primary">Places Page</button>
-  </a>
-          </main>
-      </Def>
-    )
-  }
+function home() {
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+        <div id='img1'>
+          <img src="\images\Thai.jpg" alt="Chia Fruit Shake" />
+          <div>
+            Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on {" "}
+            <a href="UNSPLASH_LINK">Unsplash</a>
+          </div>
+        </div>
+        <a href="/places">
+          <button className="btn-primary">Places Page</button>
+        </a>
+      </main>
 
+    </Def>
+  )
+}
 
 module.exports = home
