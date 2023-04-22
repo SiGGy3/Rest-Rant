a473b8559b190395eadaeab7b7e68afd41c1c4e2
SiGGy3 <justinjwsig@gmail.com>
2023-04-22 13:40:09 -0700

    delete functionality implimented

--- Views/places/show.jsx
+++ Views/places/show.jsx
@@ -1,28 +1,22 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if (!places[id]) {
-      res.render('error404')
-    }
-    else {
-      res.render('places/show')
-    }
-  })
-      
-
-function show () {
-    return (
-        <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
-        </Def>
-    )
+function show(data) {
+  return (
+    <Def>
+      <main>
+        <h1>{data.place.name}</h1>
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+          Edit
+        </a>
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
+      </main>
+    </Def>
+  );
 }
 
-module.exports = show
+module.exports = show;
