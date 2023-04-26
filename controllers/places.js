60be3064a91abde2f66aca1437a7771e6aebab08
SiGGy3 <justinjwsig@gmail.com>
2023-04-25 22:10:49 -0700

    removed mock array from controllers and fixed img

--- controllers/places.js
+++ controllers/places.js
@@ -6,20 +6,6 @@ router.get('/', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai-food.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffe-cat.jpg'
-      }]
-
       res.render('places/index', {places})
 })
 
